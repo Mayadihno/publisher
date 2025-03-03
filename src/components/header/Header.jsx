2dd563dcd9ba848c012d7eb461b3801b6cd2bd40
Maya Tunde Olawale <mayadihno@gmail.com>
2025-03-03 07:50:29 +0100

    add login modal to Header component; implement state management for email and password inputs

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { ICONS } from "../../static/icons/icons";
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [visible, setVisible] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
   return (
     <React.Fragment>
       <div
@@ -14,7 +23,12 @@ const Header = () => {
             Welcome to our Publishing House!
           </h3>
           <div className="flex justify-between items-center">
-            <h5 className="text-[#797979] text-base font-semibold">Login</h5>
+            <h5
+              onClick={() => setShow(true)}
+              className="text-white bg-[#DE324D] rounded-sm shadow-md px-3 py-1 cursor-pointer text-base font-semibold"
+            >
+              Login
+            </h5>
             <div
               data-aos="fade-up"
               data-aos-duration="2000"
@@ -62,6 +76,78 @@ const Header = () => {
           </h5>
         </div>
       </div>
+      {show && (
+        <div className="fixed w-full h-screen top-0 left-0 bg-[#00000030] z-80 flex justify-center items-center">
+          <div className="relative w-[95%] md:w-[30%] h-[38vh] md:h-[50vh] bg-white rounded-md p-4 shadow-sm md:mt-0 mt-[-50px] ">
+            <ICONS.close
+              size={30}
+              className="absolute cursor-pointer right-3 top-3 z-50"
+              onClick={() => setShow(false)}
+            />
+
+            <div className="pt-10">
+              <h3 className="text-xl font-[500] text-center text-black">
+                Admin Login
+              </h3>
+              <form onSubmit={handleLogin} className="pt-4">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="px-4 py-3 outline-none rounded-md shadow-sm bg-[#F5F5F4] w-full"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <div className="relative pt-6">
+                  <input
+                    type={!visible ? "password" : "text"}
+                    autoComplete="current-password"
+                    placeholder="Password"
+                    className="px-4 py-3 outline-none rounded-md shadow-sm bg-[#F5F5F4] w-full"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  {visible ? (
+                    <ICONS.eyelock
+                      className="absolute top-9 right-2 cursor-pointer"
+                      size={20}
+                      onClick={() => setVisible(false)}
+                    />
+                  ) : (
+                    <ICONS.eye
+                      className="absolute top-9 right-2 cursor-pointer"
+                      size={20}
+                      onClick={() => setVisible(true)}
+                    />
+                  )}
+                </div>
+                <div className="flex justify-between items-center pt-2">
+                  <div className="flex items-center">
+                    <input
+                      type="checkbox"
+                      name="checked"
+                      className="md:mt-1 mt-0 w-[15px] h-[15px]"
+                    />
+                    <p className="pl-2 md:text-sm text-xs">Remember me</p>
+                  </div>
+                  <h3 className="text-sm cursor-pointer font-[500] text-[#DE324D]">
+                    Forget Password?
+                  </h3>
+                </div>
+                <div className="md:mt-5 mt-6 flex justify-center">
+                  <button
+                    type="submit"
+                    className="px-8 uppercase py-3 bg-[#000] font-[500] cursor-pointer hover:bg-[#DC143C] rounded-sm text-white"
+                  >
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
     </React.Fragment>
   );
 };
